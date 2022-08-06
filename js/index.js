63c5f1181e0ce66bd5d93bd6d749aa6a9893c63c
Cody Clements <subliminalverses2@gmail.com>
2022-08-05 20:42:14 -0500

    create a function to clear elements

--- js/index.js
+++ js/index.js
@@ -2,9 +2,20 @@ const sketchPad = document.querySelector('.sketch-pad');
 sketchPad.style.gridTemplateColumns = 'repeat(16, 1fr)';
 sketchPad.style.gridTemplateRows = 'repeat(16, 1fr)';
 
-for(let i = 0; 256 > i; i++){
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    sketchPad.appendChild(tile);
+
+function renderTiles() {
+
+    clearElement(sketchPad);
+
+    for (let i = 0; 256 > i; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        sketchPad.appendChild(tile);
+    }
 }
 
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
