092cf181843a3e872e4ffed47cdf6b6f8264e222
Cody Clements <subliminalverses2@gmail.com>
2022-08-05 22:46:43 -0500

    add function to change board size

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 const sketchPad = document.querySelector('.sketch-pad');
 const btns = document.querySelectorAll('button');
-sketchPad.style.gridTemplateColumns = 'repeat(16, 1fr)';
-sketchPad.style.gridTemplateRows = 'repeat(16, 1fr)';
+const slider = document.querySelector('#slider');
+const sliderValueContainer = document.querySelector('.slider-value-container');
 
 let activeColor = 'black';
+let numberOfTiles = '16';
+
+sketchPad.style.gridTemplateColumns = `repeat(${numberOfTiles}, 1fr)`;
+sketchPad.style.gridTemplateRows = `repeat(${numberOfTiles}, 1fr)`;
+
+
 
 function renderTiles() {
 
     clearElement(sketchPad);
 
-    for (let i = 0; 256 > i; i++) {
+    for (let i = 0; numberOfTiles * numberOfTiles > i; i++) {
         const tile = document.createElement('div');
         tile.classList.add('tile');
         sketchPad.appendChild(tile);
@@ -31,11 +37,11 @@ sketchPad.addEventListener('pointerover', (e) => {
 btns.forEach(btn => {
     btn.addEventListener('click', (e) => {
         deActivateBtn()
-        if(e.target.innerText.toLowerCase() === 'eraser'){
+        if (e.target.innerText.toLowerCase() === 'eraser') {
             activeColor = 'white';
-        }else {    
-        activeColor = e.target.innerText.toLowerCase();
-        e.target.style.border = `3px solid ${activeColor}`
+        } else {
+            activeColor = e.target.innerText.toLowerCase();
+            e.target.style.border = `3px solid ${activeColor}`
         }
     })
 })
@@ -45,5 +51,13 @@ function deActivateBtn() {
         btn.style.border = 'none';
     })
 }
+
+slider.addEventListener('input', () => {
+    sliderValueContainer.innerText = `${slider.value} X ${slider.value}`
+    numberOfTiles = slider.value.toString();
+    sketchPad.style.gridTemplateColumns = `repeat(${numberOfTiles}, 1fr)`;
+    sketchPad.style.gridTemplateRows = `repeat(${numberOfTiles}, 1fr)`;
+    renderTiles()
+})
 
 renderTiles()
